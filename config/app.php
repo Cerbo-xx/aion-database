1d511b9f9e4c974e9865ca1002c820ea8eaec0f6
sipherion <sipherion@gmail.com>
2017-04-04 14:12:10 +0200

    Add application providers and aliases

--- config/app.php
+++ config/app.php
@@ -167,7 +167,7 @@ return [
          * Package Service Providers...
          */
 
-        //
+        Barryvdh\Debugbar\ServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -177,6 +177,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+		Collective\Html\HtmlServiceProvider::class,
 
     ],
 
@@ -225,6 +226,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+		'Form'  => Collective\Html\FormFacade::class,
+		'Html'  => Collective\Html\HtmlFacade::class,
 
     ],
 
