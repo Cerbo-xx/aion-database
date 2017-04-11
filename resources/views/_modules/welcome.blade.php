5a8f1e24b4a7f2fb66746f077a35552a60e91a90
sipherion <sipherion@gmail.com>
2017-04-11 12:46:19 +0200

    Neutralize Welcome page content

--- resources/views/_modules/welcome.blade.php
+++ resources/views/_modules/welcome.blade.php
@@ -5,14 +5,9 @@
 	<div id="welcomeDiv" class="col-md-12">
 		<img src="img/logo.png" alt="{{ Config::get('aion.website_name') }} logo">
 		<h3>@lang('general.welcome.title', ['version' => '2.7', 'siteName' => Config::get('aion.website_name')])</h3>
-		<h4>Il s'agit d'une Database pour Aion qui ne contient que des objets de la version 2.7 issus du client Gameforge 2.7.0.2</h4>
-		<p>Cette base de données est en construction constante, n'hésitez pas à nous faire vos suggestions
-		directement sur le forum.<br/>Merci à tous ceux qui participent au projet !</p>
-		<hr/>
-		<p>Cette database est réalisée par des passionés qui ont malheureusement un travail à côté ; il est donc
-		possible que les mises à jour (incluant correction de bug de la database elle-même ou ajout / suppression /
-		modification de contenu) sur cette database prennent du temps. Vous pouvez bien évidemment participer
-		à son élaboration, dans ce cas veuillez contacter Thane sur le forum de Chanology.</p>
+		<!-- Your content bellow this comment -->
+		<p>
+		</p>
 	</div>
 </div>
 @endsection
